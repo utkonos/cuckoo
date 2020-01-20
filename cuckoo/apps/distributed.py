df6127741f148fcb7f0c509998639c4e7d08608c
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-19 22:56:42 -0500

    Fix linter problems.

--- cuckoo/apps/distributed.py
+++ cuckoo/apps/distributed.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2016-2017 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 import os
 
 from cuckoo.distributed.app import create_app
@@ -12,21 +11,24 @@ from cuckoo.misc import decide_cwd
 
 app = None
 
+
 def cuckoo_distributed(hostname, port, debug):
     app = create_app()
     app.run(host=hostname, port=port, debug=debug)
+
 
 def cuckoo_distributed_instance(name):
     app = create_app()
 
     with app.app_context():
-        if name == "dist.scheduler":
+        if name == 'dist.scheduler':
             scheduler()
-        elif name == "dist.status":
+        elif name == 'dist.status':
             status_caching()
         else:
             handle_node(name)
 
-if os.environ.get("CUCKOO_APP") == "dist":
+
+if os.environ.get('CUCKOO_APP') == 'dist':
     decide_cwd(exists=True)
     app = create_app()
