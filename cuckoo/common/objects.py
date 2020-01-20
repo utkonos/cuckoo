73aebd74658873241a0b8375e817b09ebea5b1c1
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-20 01:20:49 -0500

    Fix linter problems

--- cuckoo/common/objects.py
+++ cuckoo/common/objects.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2014-2018 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 import base64
 import binascii
 import hashlib
@@ -26,36 +25,37 @@ except ImportError:
 
 log = logging.getLogger(__name__)
 
-FILE_CHUNK_SIZE = 16*1024*1024
+FILE_CHUNK_SIZE = 16 * 1024 * 1024
 
 URL_REGEX = (
     # HTTP/HTTPS.
-    "(https?:\\/\\/)"
-    "((["
+    '(https?:\\/\\/)'
+    '((['
     # IP address.
-    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
-    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
-    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
-    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])]|"
+    '(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.'
+    '(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.'
+    '(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.'
+    '(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])]|'
     # Or domain name.
-    "[a-zA-Z0-9\\.-]+)"
+    '[a-zA-Z0-9\\.-]+)'
     # Optional port.
-    "(\\:\\d+)?"
+    '(\\:\\d+)?'
     # URI.
-    "(/[\\(\\)a-zA-Z0-9_:%?=/\\.-]*)?"
+    '(/[\\(\\)a-zA-Z0-9_:%?=/\\.-]*)?'
 )
 
 PUBPRIVKEY = (
-    "("
-    "(?:-----BEGIN PUBLIC KEY-----"
-    "[a-zA-Z0-9\\n\\+/]+"
-    "-----END PUBLIC KEY-----)"
-    "|"
-    "(?:-----BEGIN RSA PRIVATE KEY-----"
-    "[a-zA-Z0-9\\n\\+/]+"
-    "-----END RSA PRIVATE KEY-----)"
-    ")"
+    '('
+    '(?:-----BEGIN PUBLIC KEY-----'
+    '[a-zA-Z0-9\\n\\+/]+'
+    '-----END PUBLIC KEY-----)'
+    '|'
+    '(?:-----BEGIN RSA PRIVATE KEY-----'
+    '[a-zA-Z0-9\\n\\+/]+'
+    '-----END RSA PRIVATE KEY-----)'
+    ')'
 )
+
 
 class Dictionary(dict):
     """Cuckoo custom dict."""
@@ -66,6 +66,7 @@ class Dictionary(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+
 class URL:
     """URL base object."""
 
@@ -73,7 +74,8 @@ class URL:
         """@param url: URL"""
         self.url = url
 
-class File(object):
+
+class File:
     """Basic file object class with all useful utilities."""
     # Given that ssdeep hashes are not really used much in practice we're just
     # going to disable its warning by default for now.
@@ -101,6 +103,7 @@ class File(object):
 
     def get_name(self):
         """Get file name.
+
         @return: file name.
         """
         file_name = os.path.basename(self.file_path)
@@ -116,14 +119,14 @@ class File(object):
 
     def get_data(self):
         """Read file contents.
+
         @return: data.
         """
         return self.file_data
 
     def get_chunks(self):
         """Read file contents in chunks (generator)."""
-
-        with open(self.file_path, "rb") as fd:
+        with open(self.file_path, 'rb') as fd:
             while True:
                 chunk = fd.read(FILE_CHUNK_SIZE)
                 if not chunk:
@@ -145,7 +148,7 @@ class File(object):
             sha256.update(chunk)
             sha512.update(chunk)
 
-        self._crc32 = "".join("%02X" % ((crc >> i) & 0xff)
+        self._crc32 = ''.join('%02X' % ((crc >> i) & 0xff)
                               for i in [24, 16, 8, 0])
         self._md5 = md5.hexdigest()
         self._sha1 = sha1.hexdigest()
@@ -155,17 +158,19 @@ class File(object):
     @property
     def file_data(self):
         if not self._file_data:
-            self._file_data = open(self.file_path, "rb").read()
+            self._file_data = open(self.file_path, 'rb').read()
         return self._file_data
 
     def get_size(self):
         """Get file size.
+
         @return: file size.
         """
         return os.path.getsize(self.file_path)
 
     def get_crc32(self):
         """Get CRC32.
+
         @return: CRC32.
         """
         if not self._crc32:
@@ -174,6 +179,7 @@ class File(object):
 
     def get_md5(self):
         """Get MD5.
+
         @return: MD5.
         """
         if not self._md5:
@@ -182,6 +188,7 @@ class File(object):
 
     def get_sha1(self):
         """Get SHA1.
+
         @return: SHA1.
         """
         if not self._sha1:
@@ -190,6 +197,7 @@ class File(object):
 
     def get_sha256(self):
         """Get SHA256.
+
         @return: SHA256.
         """
         if not self._sha256:
@@ -197,8 +205,8 @@ class File(object):
         return self._sha256
 
     def get_sha512(self):
-        """
-        Get SHA512.
+        """Get SHA512.
+
         @return: SHA512.
         """
         if not self._sha512:
@@ -207,12 +215,13 @@ class File(object):
 
     def get_ssdeep(self):
         """Get SSDEEP.
+
         @return: SSDEEP.
         """
         if not HAVE_PYDEEP:
             if not File.notified_pydeep:
                 File.notified_pydeep = True
-                log.warning("Unable to import pydeep (install with `pip install pydeep`)")
+                log.warning('Unable to import pydeep (install with `pip install pydeep`)')
             return None
 
         try:
@@ -222,6 +231,7 @@ class File(object):
 
     def get_type(self):
         """Get MIME file type.
+
         @return: file type.
         """
         return sflock.magic.from_file(
@@ -230,6 +240,7 @@ class File(object):
 
     def get_content_type(self):
         """Get MIME content file type (example: image/jpeg).
+
         @return: file content type.
         """
         return sflock.magic.from_file(
@@ -239,29 +250,29 @@ class File(object):
     def get_exported_functions(self):
         """Get the exported function names of this PE file."""
         filetype = self.get_type()
-        if "MS-DOS" not in filetype and "PE32" not in self.get_type():
+        if 'MS-DOS' not in filetype and 'PE32' not in self.get_type():
             return
 
         try:
             pe = pefile.PE(self.file_path)
-            if not hasattr(pe, "DIRECTORY_ENTRY_EXPORT"):
+            if not hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
                 return
 
             for export in pe.DIRECTORY_ENTRY_EXPORT.symbols:
                 if export.name:
                     yield export.name
         except Exception as e:
-            log.warning("Error enumerating exported functions: %s", e)
+            log.warning('Error enumerating exported functions: %s', e)
 
     def get_imported_functions(self):
         """Get the imported functions of this PE file."""
         filetype = self.get_type()
-        if "MS-DOS" not in filetype and "PE32" not in self.get_type():
+        if 'MS-DOS' not in filetype and 'PE32' not in self.get_type():
             return
 
         try:
             pe = pefile.PE(self.file_path)
-            if not hasattr(pe, "DIRECTORY_ENTRY_IMPORT"):
+            if not hasattr(pe, 'DIRECTORY_ENTRY_IMPORT'):
                 return
 
             for imp in pe.DIRECTORY_ENTRY_IMPORT:
@@ -272,52 +283,52 @@ class File(object):
                                hint=entry.hint,
                                address=entry.address)
         except Exception as e:
-            log.warning("Error enumerating imported functions: %s", e)
+            log.warning('Error enumerating imported functions: %s', e)
 
     def get_apk_entry(self):
-        """Get the entry point for this APK. The entry point is denoted by a
-        package and main activity name."""
+        """Get the entry point for this APK. The entry point is denoted by a package and main activity name."""
         filetype = self.get_type()
-        if "Zip archive data" not in filetype and "Java archive data" not in filetype:
-            return "", ""
+        if 'Zip archive data' not in filetype and 'Java archive data' not in filetype:
+            return '', ''
 
         from androguard.core.bytecodes.apk import APK
 
         try:
             a = APK(self.file_path)
             if not a.is_valid_APK():
-                return "", ""
+                return '', ''
 
             package = a.get_package()
             if not package:
-                log.warning("Unable to find the main package, this analysis "
-                            "will probably fail.")
-                return "", ""
+                log.warning('Unable to find the main package, this analysis '
+                            'will probably fail.')
+                return '', ''
 
             main_activity = a.get_main_activity()
             if main_activity:
-                log.debug("Picked package %s and main activity %s.",
+                log.debug('Picked package %s and main activity %s.',
                           package, main_activity)
                 return package, main_activity
 
             activities = a.get_activities()
             for activity in activities:
-                if "main" in activity or "start" in activity:
-                    log.debug("Choosing package %s and main activity due to "
-                              "its name %s.", package, activity)
+                if 'main' in activity or 'start' in activity:
+                    log.debug('Choosing package %s and main activity due to '
+                              'its name %s.', package, activity)
                     return package, activity
 
             if activities and activities[0]:
-                log.debug("Picked package %s and the first activity %s.",
+                log.debug('Picked package %s and the first activity %s.',
                           package, activities[0])
                 return package, activities[0]
         except Exception as e:
-            log.warning("Error extracting package and main activity: %s.", e)
+            log.warning('Error extracting package and main activity: %s.', e)
 
-        return "", ""
+        return '', ''
 
-    def get_yara(self, category="binaries", externals=None):
+    def get_yara(self, category='binaries', externals=None):
         """Get Yara signatures matches.
+
         @return: matched Yara signatures.
         """
         if not os.path.getsize(self.file_path):
@@ -330,8 +341,8 @@ class File(object):
             assert len(str(self.file_path)) == len(self.file_path)
         except (UnicodeEncodeError, AssertionError):
             log.warning(
-                "Can't run Yara rules on %r as Unicode paths are currently "
-                "not supported in combination with Yara!", self.file_path
+                'Can\'t run Yara rules on %r as Unicode paths are currently '
+                'not supported in combination with Yara!', self.file_path
             )
             return []
 
@@ -340,36 +351,36 @@ class File(object):
             strings, offsets = set(), {}
             for _, key, value in match.strings:
                 strings.add(base64.b64encode(value))
-                offsets[key.lstrip("$")] = []
+                offsets[key.lstrip('$')] = []
 
             strings = sorted(strings)
             for offset, key, value in match.strings:
-                offsets[key.lstrip("$")].append(
+                offsets[key.lstrip('$')].append(
                     (offset, strings.index(base64.b64encode(value)))
                 )
 
             meta = {
-                "description": "(no description)",
+                'description': '(no description)',
             }
             meta.update(match.meta)
 
             results.append({
-                "name": match.rule,
-                "meta": meta,
-                "strings": strings,
-                "offsets": offsets,
+                'name': match.rule,
+                'meta': meta,
+                'strings': strings,
+                'offsets': offsets,
             })
 
         return results
 
     def mmap(self, fileno):
-        if hasattr(mmap, "PROT_READ"):
+        if hasattr(mmap, 'PROT_READ'):
             access = mmap.PROT_READ
-        elif hasattr(mmap, "ACCESS_READ"):
+        elif hasattr(mmap, 'ACCESS_READ'):
             access = mmap.ACCESS_READ
         else:
             log.warning(
-                "Regexing through a file is not supported on your OS!"
+                'Regexing through a file is not supported on your OS!'
             )
             return
 
@@ -381,10 +392,10 @@ class File(object):
             return []
 
         # http://stackoverflow.com/a/454589
-        urls, f = set(), open(self.file_path, "rb")
+        urls, f = set(), open(self.file_path, 'rb')
         for url in re.findall(URL_REGEX, self.mmap(f.fileno())):
             if not is_whitelisted_domain(url[1]):
-                urls.add("".join(url))
+                urls.add(''.join(url))
         return list(urls)
 
     def get_keys(self):
@@ -392,39 +403,42 @@ class File(object):
         if not os.path.getsize(self.file_path):
             return []
 
-        f = open(self.file_path, "rb")
+        f = open(self.file_path, 'rb')
         return list(set(re.findall(PUBPRIVKEY, self.mmap(f.fileno()))))
 
     def get_all(self):
         """Get all information available.
+
         @return: information dict.
         """
         infos = {}
-        infos["name"] = self.get_name()
-        infos["path"] = self.file_path
-        infos["size"] = self.get_size()
-        infos["crc32"] = self.get_crc32()
-        infos["md5"] = self.get_md5()
-        infos["sha1"] = self.get_sha1()
-        infos["sha256"] = self.get_sha256()
-        infos["sha512"] = self.get_sha512()
-        infos["ssdeep"] = self.get_ssdeep()
-        infos["type"] = self.get_type()
-        infos["yara"] = self.get_yara()
-        infos["urls"] = self.get_urls()
+        infos['name'] = self.get_name()
+        infos['path'] = self.file_path
+        infos['size'] = self.get_size()
+        infos['crc32'] = self.get_crc32()
+        infos['md5'] = self.get_md5()
+        infos['sha1'] = self.get_sha1()
+        infos['sha256'] = self.get_sha256()
+        infos['sha512'] = self.get_sha512()
+        infos['ssdeep'] = self.get_ssdeep()
+        infos['type'] = self.get_type()
+        infos['yara'] = self.get_yara()
+        infos['urls'] = self.get_urls()
         return infos
 
-class Archive(object):
+
+class Archive:
     def __init__(self, filepath):
         self.filepath = filepath
         self.z = zipfile.ZipFile(filepath)
 
     def get_file(self, filename):
         filepath = tempfile.mktemp()
-        shutil.copyfileobj(self.z.open(filename), open(filepath, "wb"))
+        shutil.copyfileobj(self.z.open(filename), open(filepath, 'wb'))
         return File(filepath, temporary=True)
 
-class Buffer(object):
+
+class Buffer:
     """A brief wrapper around string buffers for quick Yara rule matching."""
 
     def __init__(self, buffer):
@@ -436,17 +450,18 @@ class Buffer(object):
             results.append(match.rule)
         return results
 
-class YaraMatch(object):
+
+class YaraMatch:
     def __init__(self, match, category=None):
-        self.name = match["name"]
-        self.meta = match["meta"]
+        self.name = match['name']
+        self.meta = match['meta']
         self._decoded = {}
-        self.offsets = match["offsets"]
+        self.offsets = match['offsets']
         self.category = category
 
         self._strings = []
-        for s in match["strings"]:
-            self._strings.append(s.decode("base64"))
+        for s in match['strings']:
+            self._strings.append(s.decode('base64'))
 
     def string(self, identifier, index=0):
         off, idx = self.offsets[identifier][index]
@@ -458,17 +473,18 @@ class YaraMatch(object):
             ret.append(self._strings[idx])
         return ret
 
-class ExtractedMatch(object):
+
+class ExtractedMatch:
     def __init__(self, match):
-        self.category = match["category"]
-        self.program = match.get("program")
-        self.first_seen = match.get("first_seen")
-        self.pid = match.get("pid")
+        self.category = match['category']
+        self.program = match.get('program')
+        self.first_seen = match.get('first_seen')
+        self.pid = match.get('pid')
 
         self.yara = []
-        for ym in match["yara"]:
+        for ym in match['yara']:
             self.yara.append(YaraMatch(ym))
 
         # Raw payload.
-        self.raw = match.get("raw")
-        self.info = match["info"]
+        self.raw = match.get('raw')
+        self.info = match['info']
