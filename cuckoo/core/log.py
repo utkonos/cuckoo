09ab80a407b34b01e1e04d25879be9fd110c0db6
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-20 02:14:32 -0500

    Fix linter problems

--- cuckoo/core/log.py
+++ cuckoo/core/log.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2016-2019 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 import copy
 import json
 import logging
@@ -32,21 +31,25 @@ else:
 # so we can (ab)use this for both multi-threading and Gevent code
 task_key = gevent.thread.get_ident
 
+
 class DatabaseHandler(logging.Handler):
     """Logging to database handler.
+
     Used to log errors related to tasks in database.
     """
 
     def emit(self, record):
         # TODO Should this also attempt to guess the task ID from _tasks?
-        if hasattr(record, "task_id"):
+        if hasattr(record, 'task_id'):
             Database().add_error(
                 self.format(record), int(record.task_id),
-                getattr(record, "error_action", None)
+                getattr(record, 'error_action', None)
             )
+
 
 class TaskHandler(logging.Handler):
     """Per-task logger.
+
     Used to log all task specific events to a per-task cuckoo.log log file.
     """
 
@@ -55,7 +58,8 @@ class TaskHandler(logging.Handler):
         if not task:
             return
 
-        task[1].write("%s\n" % self.format(record))
+        task[1].write('%s\n' % self.format(record))
+
 
 class ConsoleHandler(logging.StreamHandler):
     """Logging to console handler."""
@@ -63,33 +67,33 @@ class ConsoleHandler(logging.StreamHandler):
     def emit(self, record):
         colored = copy.copy(record)
 
-        if record.levelname == "WARNING":
+        if record.levelname == 'WARNING':
             colored.msg = yellow(record.msg)
-        elif record.levelname == "ERROR" or record.levelname == "CRITICAL":
+        elif record.levelname == 'ERROR' or record.levelname == 'CRITICAL':
             colored.msg = red(record.msg)
         else:
-            if "analysis procedure completed" in record.msg:
+            if 'analysis procedure completed' in record.msg:
                 colored.msg = cyan(record.msg)
             else:
                 colored.msg = record.msg
 
         logging.StreamHandler.emit(self, colored)
 
+
 class JsonFormatter(logging.Formatter):
     """Logging Cuckoo logs to JSON."""
-
     def format(self, record):
-        action = record.__dict__.get("action")
-        status = record.__dict__.get("status")
+        action = record.__dict__.get('action')
+        status = record.__dict__.get('status')
         task = _tasks.get(task_key())
-        task_id = task[0] if task else record.__dict__.get("task_id")
+        task_id = task[0] if task else record.__dict__.get('task_id')
         d = {
-            "action": action,
-            "task_id": task_id,
-            "status": status,
-            "time": record.created,
-            "level": record.levelname.lower(),
-            "message": record.getMessage(),
+            'action': action,
+            'task_id': task_id,
+            'status': status,
+            'time': record.created,
+            'level': record.levelname.lower(),
+            'message': record.getMessage(),
         }
         base = logging.makeLogRecord({})
         for key, value in record.__dict__.items():
@@ -98,9 +102,10 @@ class JsonFormatter(logging.Formatter):
         return json.dumps(d)
 
     def filter(self, record):
-        action = record.__dict__.get("action")
-        status = record.__dict__.get("status")
+        action = record.__dict__.get('action')
+        status = record.__dict__.get('status')
         return action and status
+
 
 def task_log_start(task_id):
     """Associate a thread with a task."""
@@ -112,7 +117,7 @@ def task_log_start(task_id):
                 return
 
             _task_threads[task_id] = []
-            fp = open(os.path.join(task_path, "cuckoo.log"), "a+b")
+            fp = open(os.path.join(task_path, 'cuckoo.log'), 'a+b')
             _tasks[task_key()] = (task_id, fp)
         else:
             existing_key = _task_threads[task_id][0]
@@ -121,6 +126,7 @@ def task_log_start(task_id):
         _task_threads[task_id].append(task_key())
     finally:
         _tasks_lock.release()
+
 
 def task_log_stop(task_id):
     """Disassociate a thread from a task."""
@@ -138,52 +144,54 @@ def task_log_stop(task_id):
     finally:
         _tasks_lock.release()
 
+
 def init_logger(name, level=None):
     formatter = logging.Formatter(
-        "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
+        '%(asctime)s [%(name)s] %(levelname)s: %(message)s'
     )
 
-    if name == "cuckoo.log":
-        l = logging.handlers.WatchedFileHandler(cwd("log", "cuckoo.log"))
-        l.setFormatter(formatter)
-        l.setLevel(level)
+    if name == 'cuckoo.log':
+        lh = logging.handlers.WatchedFileHandler(cwd('log', 'cuckoo.log'))
+        lh.setFormatter(formatter)
+        lh.setLevel(level)
 
-    if name == "cuckoo.json":
+    if name == 'cuckoo.json':
         j = JsonFormatter()
-        l = logging.handlers.WatchedFileHandler(cwd("log", "cuckoo.json"))
-        l.setFormatter(j)
-        l.addFilter(j)
+        lh = logging.handlers.WatchedFileHandler(cwd('log', 'cuckoo.json'))
+        lh.setFormatter(j)
+        lh.addFilter(j)
 
-    if name == "console":
-        l = ConsoleHandler()
-        l.setFormatter(formatter)
-        l.setLevel(level)
+    if name == 'console':
+        lh = ConsoleHandler()
+        lh.setFormatter(formatter)
+        lh.setLevel(level)
 
-    if name == "database":
-        l = DatabaseHandler()
-        l.setLevel(logging.ERROR)
+    if name == 'database':
+        lh = DatabaseHandler()
+        lh.setLevel(logging.ERROR)
 
-    if name == "task":
-        l = TaskHandler()
-        l.setFormatter(formatter)
+    if name == 'task':
+        lh = TaskHandler()
+        lh.setFormatter(formatter)
 
-    if name.startswith("process-") and name.endswith(".json"):
+    if name.startswith('process-') and name.endswith('.json'):
         j = JsonFormatter()
-        l = logging.handlers.WatchedFileHandler(cwd("log", name))
-        l.setFormatter(j)
-        l.addFilter(j)
+        lh = logging.handlers.WatchedFileHandler(cwd('log', name))
+        lh.setFormatter(j)
+        lh.addFilter(j)
 
-    _loggers[name] = l
-    logging.getLogger().addHandler(l)
+    _loggers[name] = lh
+    logging.getLogger().addHandler(lh)
+
 
 def logger(message, *args, **kwargs):
     """Log a message to specific logger instance."""
-    logfile = kwargs.pop("logfile", None)
+    logfile = kwargs.pop('logfile', None)
     record = logging.LogRecord(
         None, logging.INFO, None, None, message, args, None, None
     )
-    record.asctime = "%s,%03d" % (
-        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created)),
+    record.asctime = '%s,%03d' % (
+        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(record.created)),
         record.msecs
     )
     record.message = record.getMessage()
@@ -192,5 +200,5 @@ def logger(message, *args, **kwargs):
     for key, value in _loggers.items():
         if logfile and key == logfile:
             value.handle(record)
-        if logfile is None and key.endswith(".json"):
+        if logfile is None and key.endswith('.json'):
             value.handle(record)
