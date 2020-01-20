2de04fb101ea1d4703e0a25a8745780027d4191c
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-20 01:13:34 -0500

    Fix linter problems

--- cuckoo/common/netlog.py
+++ cuckoo/common/netlog.py
@@ -2,20 +2,18 @@
 # Copyright (C) 2014-2019 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 import bson
 import datetime
 import hashlib
 import logging
-import os.path
 import struct
 
 # The BSON module provided by pymongo works through its "BSON" class.
-if hasattr(bson, "BSON"):
+if hasattr(bson, 'BSON'):
     bson_decode = lambda d: bson.BSON(d).decode()
 # The BSON module provided by "pip install bson" works through the
 # "loads" function (just like pickle etc.)
-elif hasattr(bson, "loads"):
+elif hasattr(bson, 'loads'):
     bson_decode = lambda d: bson.loads(d)
 
 from cuckoo.common.files import Storage
@@ -27,20 +25,24 @@ log = logging.getLogger(__name__)
 # 20 Mb max message length.
 MAX_MESSAGE_LENGTH = 20 * 1024 * 1024
 
+
 def pointer_converter_32bit(v):
-    return "0x%08x" % (v % 2**32)
+    return '0x%08x' % (v % 2**32)
+
 
 def pointer_converter_64bit(v):
-    return "0x%016x" % (v % 2**64)
+    return '0x%016x' % (v % 2**64)
+
 
 def default_converter_32bit(v):
-    if isinstance(v, (int, long)) and v < 0:
+    if isinstance(v, (int)) and v < 0:
         return v % 2**32
 
     # Try to avoid various unicode issues through usage of latin-1 encoding.
     if isinstance(v, str):
-        return v.decode("latin-1")
+        return v.decode('latin-1')
     return v
+
 
 def default_converter_64bit(v):
     # Don't convert signed 64-bit integers into unsigned 64-bit integers as
@@ -51,10 +53,11 @@ def default_converter_64bit(v):
 
     # Try to avoid various unicode issues through usage of latin-1 encoding.
     if isinstance(v, str):
-        return v.decode("latin-1")
+        return v.decode('latin-1')
     return v
 
-class BsonParser(object):
+
+class BsonParser:
     """Interprets .bson logs from the monitor.
 
     The monitor provides us with "info" messages that explain how the function
@@ -66,14 +69,14 @@ class BsonParser(object):
     """
     converters_32bit = {
         None: default_converter_32bit,
-        "p": pointer_converter_32bit,
-        "x": pointer_converter_32bit,
+        'p': pointer_converter_32bit,
+        'x': pointer_converter_32bit,
     }
 
     converters_64bit = {
         None: default_converter_64bit,
-        "p": pointer_converter_64bit,
-        "x": pointer_converter_32bit,
+        'p': pointer_converter_64bit,
+        'x': pointer_converter_32bit,
     }
 
     def __init__(self, fd, task_id=None):
@@ -114,7 +117,7 @@ class BsonParser(object):
                 if (value & key) == key:
                     flags[argument].append(flag)
 
-            flags[argument] = "|".join(flags[argument])
+            flags[argument] = '|'.join(flags[argument])
 
     def determine_unserializers(self, arginfo):
         """Determine which unserializers (or converters) have to be used in
@@ -147,98 +150,98 @@ class BsonParser(object):
                 return
 
             if len(data) != 4:
-                log.critical("BsonParser lacking data.")
+                log.critical('BsonParser lacking data.')
                 return
 
-            blen = struct.unpack("I", data)[0]
+            blen = struct.unpack('I', data)[0]
             if blen > MAX_MESSAGE_LENGTH:
                 log.critical(
-                    "BSON message larger than MAX_MESSAGE_LENGTH, "
-                    "stopping handler."
+                    'BSON message larger than MAX_MESSAGE_LENGTH, '
+                    'stopping handler.'
                 )
                 return
 
-            data += self.fd.read(blen-4)
+            data += self.fd.read(blen - 4)
             if len(data) < blen:
-                log.critical("BsonParser lacking data.")
+                log.critical('BsonParser lacking data.')
                 return
 
             try:
                 dec = bson_decode(data)
             except Exception as e:
                 log.warning(
-                    "BsonParser decoding problem %s on data[:50] %s",
+                    'BsonParser decoding problem %s on data[:50] %s',
                     e, repr(data[:50])
                 )
                 return
 
-            mtype = dec.get("type", "none")
-            index = dec.get("I", -1)
+            mtype = dec.get('type', 'none')
+            index = dec.get('I', -1)
 
-            if mtype == "info":
+            if mtype == 'info':
                 # API call index info message, explaining the argument names, etc.
-                name = dec.get("name", "NONAME")
-                arginfo = dec.get("args", [])
-                category = dec.get("category")
+                name = dec.get('name', 'NONAME')
+                arginfo = dec.get('args', [])
+                category = dec.get('category')
 
                 argnames, converters = self.determine_unserializers(arginfo)
                 self.infomap[index] = name, arginfo, argnames, converters, category
 
-                if dec.get("flags_value"):
+                if dec.get('flags_value'):
                     self.flags_value[name] = {}
-                    for arg, values in dec["flags_value"].items():
+                    for arg, values in dec['flags_value'].items():
                         self.flags_value[name][arg] = dict(values)
 
-                if dec.get("flags_bitmask"):
+                if dec.get('flags_bitmask'):
                     self.flags_bitmask[name] = {}
-                    for arg, values in dec["flags_bitmask"].items():
+                    for arg, values in dec['flags_bitmask'].items():
                         self.flags_bitmask[name][arg] = values
                 continue
 
             # Handle dumped buffers.
-            if mtype == "buffer":
-                buf = dec.get("buffer")
-                sha1 = dec.get("checksum")
+            if mtype == 'buffer':
+                buf = dec.get('buffer')
+                sha1 = dec.get('checksum')
                 self.buffer_sha1 = hashlib.sha1(buf).hexdigest()
 
                 # Why do we pass along a sha1 checksum again?
                 if sha1 != self.buffer_sha1:
-                    log.warning("Incorrect sha1 passed along for a buffer.")
+                    log.warning('Incorrect sha1 passed along for a buffer.')
 
                 filepath = cwd(
-                    "buffer", self.buffer_sha1, analysis=self.task_id
+                    'buffer', self.buffer_sha1, analysis=self.task_id
                 )
-                with open(filepath, "wb") as f:
+                with open(filepath, 'wb') as f:
                     f.write(buf)
 
                 continue
 
-            tid = dec.get("T", 0)
-            time = dec.get("t", 0)
+            tid = dec.get('T', 0)
+            time = dec.get('t', 0)
 
             parsed = {
-                "type": mtype,
-                "tid": tid,
-                "time": time,
+                'type': mtype,
+                'tid': tid,
+                'time': time,
             }
 
-            if mtype == "debug":
-                parsed["message"] = dec.get("msg", "")
-                log.info("Debug message from monitor: %s", parsed["message"])
+            if mtype == 'debug':
+                parsed['message'] = dec.get('msg', '')
+                log.info('Debug message from monitor: %s', parsed['message'])
             else:
                 # Regular api call from monitor
                 if index not in self.infomap:
-                    log.warning("Got API with unknown index - monitor needs "
-                                "to explain first: {0}".format(dec))
+                    log.warning('Got API with unknown index - monitor needs '
+                                'to explain first: {0}'.format(dec))
                     continue
 
                 apiname, arginfo, argnames, converters, category = self.infomap[index]
-                args = dec.get("args", [])
+                args = dec.get('args', [])
 
                 if len(args) != len(argnames):
                     log.warning(
-                        "Inconsistent arg count (compared to arg names) "
-                        "on %s: %s names %s", dec, argnames, apiname
+                        'Inconsistent arg count (compared to arg names) '
+                        'on %s: %s names %s', dec, argnames, apiname
                     )
                     continue
 
@@ -247,90 +250,90 @@ class BsonParser(object):
                     argdict[argnames[idx]] = converters[idx](value)
 
                 # Special new process message from the monitor.
-                if apiname == "__process__":
-                    parsed["type"] = "process"
+                if apiname == '__process__':
+                    parsed['type'] = 'process'
 
-                    if "TimeLow" in argdict:
-                        timelow = argdict["TimeLow"]
-                        timehigh = argdict["TimeHigh"]
+                    if 'TimeLow' in argdict:
+                        timelow = argdict['TimeLow']
+                        timehigh = argdict['TimeHigh']
 
-                        parsed["pid"] = pid = argdict["ProcessIdentifier"]
-                        parsed["ppid"] = argdict["ParentProcessIdentifier"]
-                        modulepath = argdict["ModulePath"]
+                        parsed['pid'] = pid = argdict['ProcessIdentifier']
+                        parsed['ppid'] = argdict['ParentProcessIdentifier']
+                        modulepath = argdict['ModulePath']
 
-                    elif "time_low" in argdict:
-                        timelow = argdict["time_low"]
-                        timehigh = argdict["time_high"]
+                    elif 'time_low' in argdict:
+                        timelow = argdict['time_low']
+                        timehigh = argdict['time_high']
 
-                        if "pid" in argdict:
-                            parsed["pid"] = pid = argdict["pid"]
-                            parsed["ppid"] = argdict["ppid"]
+                        if 'pid' in argdict:
+                            parsed['pid'] = pid = argdict['pid']
+                            parsed['ppid'] = argdict['ppid']
                         else:
-                            parsed["pid"] = pid = argdict["process_identifier"]
-                            parsed["ppid"] = argdict["parent_process_identifier"]
+                            parsed['pid'] = pid = argdict['process_identifier']
+                            parsed['ppid'] = argdict['parent_process_identifier']
 
-                        modulepath = argdict["module_path"]
+                        modulepath = argdict['module_path']
 
                     else:
                         raise CuckooResultError(
-                            "I don't recognize the bson log contents."
+                            'I don\'t recognize the bson log contents.'
                         )
 
                     # FILETIME is 100-nanoseconds from 1601 :/
                     vmtimeunix = (timelow + (timehigh << 32))
                     vmtimeunix = vmtimeunix / 10000000.0 - 11644473600
                     vmtime = datetime.datetime.fromtimestamp(vmtimeunix)
-                    parsed["first_seen"] = vmtime
+                    parsed['first_seen'] = vmtime
 
                     procname = Storage.get_filename_from_path(modulepath)
-                    parsed["process_path"] = modulepath
-                    parsed["process_name"] = procname
-                    parsed["command_line"] = argdict.get("command_line")
+                    parsed['process_path'] = modulepath
+                    parsed['process_name'] = procname
+                    parsed['command_line'] = argdict.get('command_line')
 
                     # Is this a 64-bit process?
-                    if argdict.get("is_64bit"):
+                    if argdict.get('is_64bit'):
                         self.is_64bit = True
 
                     # Is this process being "tracked"?
-                    parsed["track"] = bool(argdict.get("track", 1))
-                    parsed["modules"] = argdict.get("modules", {})
+                    parsed['track'] = bool(argdict.get('track', 1))
+                    parsed['modules'] = argdict.get('modules', {})
 
                     self.pid = pid
 
-                elif apiname == "__thread__":
-                    parsed["pid"] = pid = argdict["ProcessIdentifier"]
+                elif apiname == '__thread__':
+                    parsed['pid'] = pid = argdict['ProcessIdentifier']
 
-                # elif apiname == "__anomaly__":
-                    # tid = argdict["ThreadIdentifier"]
-                    # subcategory = argdict["Subcategory"]
-                    # msg = argdict["Message"]
+                # elif apiname == '__anomaly__':
+                    # tid = argdict['ThreadIdentifier']
+                    # subcategory = argdict['Subcategory']
+                    # msg = argdict['Message']
                     # self.handler.log_anomaly(subcategory, tid, msg)
                     # return True
-                elif apiname == "__action__":
-                    parsed["type"] = "action"
-                    parsed["action"] = argdict["action"]
+                elif apiname == '__action__':
+                    parsed['type'] = 'action'
+                    parsed['action'] = argdict['action']
                 else:
-                    parsed["type"] = "apicall"
-                    parsed["pid"] = self.pid
-                    parsed["api"] = apiname
-                    parsed["category"] = category
-                    parsed["status"] = argdict.pop("is_success", 1)
-                    parsed["return_value"] = argdict.pop("retval", 0)
-                    parsed["arguments"] = argdict
-                    parsed["flags"] = {}
+                    parsed['type'] = 'apicall'
+                    parsed['pid'] = self.pid
+                    parsed['api'] = apiname
+                    parsed['category'] = category
+                    parsed['status'] = argdict.pop('is_success', 1)
+                    parsed['return_value'] = argdict.pop('retval', 0)
+                    parsed['arguments'] = argdict
+                    parsed['flags'] = {}
 
-                    parsed["stacktrace"] = dec.get("s", [])
-                    parsed["uniqhash"] = dec.get("h", 0)
+                    parsed['stacktrace'] = dec.get('s', [])
+                    parsed['uniqhash'] = dec.get('h', 0)
 
-                    if "e" in dec and "E" in dec:
-                        parsed["last_error"] = dec["e"]
-                        parsed["nt_status"] = dec["E"]
+                    if 'e' in dec and 'E' in dec:
+                        parsed['last_error'] = dec['e']
+                        parsed['nt_status'] = dec['E']
 
                     if apiname in self.flags_value:
-                        self.resolve_flags(apiname, argdict, parsed["flags"])
+                        self.resolve_flags(apiname, argdict, parsed['flags'])
 
                     if self.buffer_sha1:
-                        parsed["buffer"] = self.buffer_sha1
+                        parsed['buffer'] = self.buffer_sha1
                         self.buffer_sha1 = None
 
             yield parsed
