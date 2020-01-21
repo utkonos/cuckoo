315569abe81af336b137a39c155dc91d9a990f12
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-20 21:36:21 -0500

    Fix linter problems

--- cuckoo/data/stuff/gatherer.py
+++ cuckoo/data/stuff/gatherer.py
@@ -1,9 +1,7 @@
 # Copyright (C) 2016-2017 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
-"""Gatherer of software components for further identification by the Cuckoo
-Sandbox team.
+"""Gatherer of software components for further identification by the Cuckoo Sandbox team.
 
 This file uploads as many relevant software components as possible so that the
 Cuckoo Sandbox team may be able to add support for more versions of certain
@@ -16,7 +14,6 @@ This command should be ran with the "pipe=cuckoo,free=yes" option. You will
 then find yourself with an analysis with all of the related dropped files.
 This analysis should be "exported" (a feature from the Cuckoo Web Interface)
 and shared with the Cuckoo Developers team in order to process the new files.
-
 """
 
 import ctypes
@@ -24,50 +21,52 @@ import glob
 import os
 
 progfiles = [
-    "Internet Explorer\\iexplore.exe",
-    "Common Files\\Microsoft Shared\\VBA\\VBA*\\VBE*.dll",
-    "Adobe\\Reader *\\Reader\\AcroRd32.dll",
-    "Adobe\\Reader *\\Reader\\AcroRd32.exe",
-    "Adobe\\Reader *\\Reader\\plug_ins\\escript.api",
+    'Internet Explorer\\iexplore.exe',
+    'Common Files\\Microsoft Shared\\VBA\\VBA*\\VBE*.dll',
+    'Adobe\\Reader *\\Reader\\AcroRd32.dll',
+    'Adobe\\Reader *\\Reader\\AcroRd32.exe',
+    'Adobe\\Reader *\\Reader\\plug_ins\\escript.api',
 ]
 
 system32 = [
-    "mshtml.dll",
-    "ncrypt.dll",
-    "jscript.dll",
-    "kernel32.dll",
-    "kernelbase.dll",
-    "ntdll.dll",
-    "advapi32.dll",
+    'mshtml.dll',
+    'ncrypt.dll',
+    'jscript.dll',
+    'kernel32.dll',
+    'kernelbase.dll',
+    'ntdll.dll',
+    'advapi32.dll',
 ]
 
+
 def report(filepath):
-    buf = "FILE_NEW:%s" % filepath
+    buf = 'FILE_NEW:%s' % filepath
     out = ctypes.create_string_buffer(512)
     value = ctypes.c_uint()
 
     ctypes.windll.kernel32.CallNamedPipeA(
-        "\\\\.\\PIPE\\cuckoo", buf, len(buf),
+        '\\\\.\\PIPE\\cuckoo', buf, len(buf),
         out, len(out), ctypes.byref(value), 1000
     )
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     filepaths = []
 
     for filepath in progfiles:
-        filepaths.append(os.path.join("C:\\Program Files", filepath))
-        filepaths.append(os.path.join("C:\\Program Files (x86)", filepath))
+        filepaths.append(os.path.join('C:\\Program Files', filepath))
+        filepaths.append(os.path.join('C:\\Program Files (x86)', filepath))
 
     for filepath in system32:
-        filepaths.append(os.path.join("C:\\Windows\\System32", filepath))
-        filepaths.append(os.path.join("C:\\Windows\\Sysnative", filepath))
+        filepaths.append(os.path.join('C:\\Windows\\System32', filepath))
+        filepaths.append(os.path.join('C:\\Windows\\Sysnative', filepath))
 
     for filepath in filepaths:
         if os.path.exists(filepath):
             report(filepath)
             continue
 
-        if "*" in filepath:
+        if '*' in filepath:
             for filepath in glob.iglob(filepath):
                 report(filepath)
             continue
