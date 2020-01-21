0474954236d1db0ce33c5f70f800a8cfffdd7750
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-21 00:06:15 -0500

    Fix linter problems

--- cuckoo/reporting/elasticsearch.py
+++ cuckoo/reporting/elasticsearch.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2016-2018 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 from __future__ import absolute_import
 
 import datetime
@@ -15,10 +14,11 @@ from cuckoo.common.elastic import elastic
 from cuckoo.common.exceptions import CuckooReportError, CuckooOperationalError
 from cuckoo.misc import cwd
 
-logging.getLogger("elasticsearch").setLevel(logging.WARNING)
-logging.getLogger("elasticsearch.trace").setLevel(logging.WARNING)
+logging.getLogger('elasticsearch').setLevel(logging.WARNING)
+logging.getLogger('elasticsearch.trace').setLevel(logging.WARNING)
 
 log = logging.getLogger(__name__)
+
 
 class ElasticSearch(Report):
     """Store report in Elasticsearch."""
@@ -26,48 +26,49 @@ class ElasticSearch(Report):
     @classmethod
     def init_once(cls):
         """Connect to Elasticsearch.
+
         @raise CuckooReportError: if unable to connect.
         """
         # Do not change these types without changing the elasticsearch
         # template as well.
-        cls.report_type = "cuckoo"
-        cls.call_type = "call"
+        cls.report_type = 'cuckoo'
+        cls.call_type = 'call'
 
         if not elastic.init():
             return
 
-        cls.template_name = "%s_template" % elastic.index
+        cls.template_name = '%s_template' % elastic.index
 
         try:
             elastic.connect()
         except CuckooOperationalError as e:
             raise CuckooReportError(
-                "Error running ElasticSearch reporting module: %s" % e
+                'Error running ElasticSearch reporting module: %s' % e
             )
 
         # check to see if the template exists apply it if it does not
         if not elastic.client.indices.exists_template(cls.template_name):
             if not cls.apply_template():
-                raise CuckooReportError("Cannot apply Elasticsearch template")
+                raise CuckooReportError('Cannot apply Elasticsearch template')
 
     @classmethod
     def apply_template(cls):
-        template_path = cwd("elasticsearch", "template.json")
+        template_path = cwd('elasticsearch', 'template.json')
         if not os.path.exists(template_path):
             return False
 
         try:
-            template = json.loads(open(template_path, "rb").read())
+            template = json.loads(open(template_path, 'rb').read())
         except ValueError:
             raise CuckooReportError(
-                "Unable to read valid JSON from the ElasticSearch "
-                "template JSON file located at: %s" % template_path
+                'Unable to read valid JSON from the ElasticSearch '
+                'template JSON file located at: %s' % template_path
             )
 
         # Create an index wildcard based off of the index name specified
         # in the config file, this overwrites the settings in
         # template.json.
-        template["template"] = elastic.index + "-*"
+        template['template'] = elastic.index + '-*'
 
         # if the template does not already exist then create it
         if not elastic.client.indices.exists_template(cls.template_name):
@@ -79,9 +80,9 @@ class ElasticSearch(Report):
     def get_base_document(self):
         # Gets precached report time and the task_id.
         header = {
-            "task_id": self.task["id"],
-            "report_time": self.report_time,
-            "report_id": self.task["id"]
+            'task_id': self.task['id'],
+            'report_time': self.report_time,
+            'report_id': self.task['id']
         }
         return header
 
@@ -99,8 +100,8 @@ class ElasticSearch(Report):
             )
         except Exception as e:
             raise CuckooReportError(
-                "Failed to save results in ElasticSearch for "
-                "task #%d: %s" % (self.task["id"], e)
+                'Failed to save results in ElasticSearch for '
+                'task #%d: %s' % (self.task['id'], e)
             )
 
     def do_bulk_index(self, bulk_reqs):
@@ -109,8 +110,8 @@ class ElasticSearch(Report):
             elasticsearch.helpers.bulk(elastic.client, bulk_reqs)
         except Exception as e:
             raise CuckooReportError(
-                "Failed to save results in ElasticSearch for "
-                "task #%d: %s" % (self.task["id"], e)
+                'Failed to save results in ElasticSearch for '
+                'task #%d: %s' % (self.task['id'], e)
             )
 
     def process_signatures(self, signatures):
@@ -119,12 +120,12 @@ class ElasticSearch(Report):
         for signature in signatures:
             new_signature = signature.copy()
 
-            if "marks" in signature:
-                new_signature["marks"] = []
-                for mark in signature["marks"]:
+            if 'marks' in signature:
+                new_signature['marks'] = []
+                for mark in signature['marks']:
                     new_mark = {}
                     for k, v in mark.iteritems():
-                        if k != "call" and type(v) == dict:
+                        if k != 'call' and type(v) == dict:
                             # If marks is a dictionary we need to explicitly define it for the ES mapping
                             # this is in the case that a key in marks is sometimes a string and sometimes a dictionary
                             # if the first document indexed into ES is a string it will not accept a signature
@@ -133,12 +134,12 @@ class ElasticSearch(Report):
                             # This presented itself in testing with signatures.marks.section which would sometimes be a
                             # PE section string such as "UPX"  and other times full details about the section as a
                             # dictionary in the case of packer_upx and packer_entropy signatures
-                            new_mark["%s_dict" % k] = v
+                            new_mark['%s_dict' % k] = v
                         else:
                             # If it is not a mark it is fine to leave key as is
                             new_mark[k] = v
 
-                    new_signature["marks"].append(new_mark)
+                    new_signature['marks'].append(new_mark)
 
             new_signatures.append(new_signature)
 
@@ -146,20 +147,20 @@ class ElasticSearch(Report):
 
     def process_behavior(self, results, bulk_submit_size=1000):
         """Index the behavioral data."""
-        for process in results.get("behavior", {}).get("processes", []):
+        for process in results.get('behavior', {}).get('processes', []):
             bulk_index = []
 
-            for call in process["calls"]:
+            for call in process['calls']:
                 base_document = self.get_base_document()
                 call_document = {
-                    "pid": process["pid"],
+                    'pid': process['pid'],
                 }
                 call_document.update(call)
                 call_document.update(base_document)
                 bulk_index.append({
-                    "_index": self.dated_index,
-                    "_type": self.call_type,
-                    "_source": call_document
+                    '_index': self.dated_index,
+                    '_type': self.call_type,
+                    '_source': call_document
                 })
                 if len(bulk_index) == bulk_submit_size:
                     self.do_bulk_index(bulk_index)
@@ -170,6 +171,7 @@ class ElasticSearch(Report):
 
     def run(self, results):
         """Index the Cuckoo report into ElasticSearch.
+
         @param results: analysis results dictionary.
         @raise CuckooReportError: if the connection or reporting failed.
         """
@@ -179,41 +181,41 @@ class ElasticSearch(Report):
 
         # Get the index time option and set the dated index accordingly
         date_index = datetime.datetime.utcnow().strftime({
-            "yearly": "%Y",
-            "monthly": "%Y-%m",
-            "daily": "%Y-%m-%d",
+            'yearly': '%Y',
+            'monthly': '%Y-%m',
+            'daily': '%Y-%m-%d',
         }[elastic.index_time_pattern])
-        self.dated_index = "%s-%s" % (elastic.index, date_index)
+        self.dated_index = '%s-%s' % (elastic.index, date_index)
 
         # Index target information, the behavioral summary, and
         # VirusTotal results.
         doc = {
-            "cuckoo_node": elastic.cuckoo_node,
-            "target": results.get("target"),
-            "summary": results.get("behavior", {}).get("summary"),
-            "info": results.get("info"),
+            'cuckoo_node': elastic.cuckoo_node,
+            'target': results.get('target'),
+            'summary': results.get('behavior', {}).get('summary'),
+            'info': results.get('info'),
         }
 
         # index elements that are not empty ES should not index blank fields
-        virustotal = results.get("virustotal")
+        virustotal = results.get('virustotal')
         if virustotal:
-            doc["virustotal"] = virustotal
+            doc['virustotal'] = virustotal
 
-        irma = results.get("irma")
+        irma = results.get('irma')
         if irma:
-            doc["irma"] = irma
+            doc['irma'] = irma
 
-        signatures = results.get("signatures")
+        signatures = results.get('signatures')
         if signatures:
-            doc["signatures"] = self.process_signatures(signatures)
+            doc['signatures'] = self.process_signatures(signatures)
 
-        dropped = results.get("dropped")
+        dropped = results.get('dropped')
         if dropped:
-            doc["dropped"] = dropped
+            doc['dropped'] = dropped
 
-        procmemory = results.get("procmemory")
+        procmemory = results.get('procmemory')
         if procmemory:
-            doc["procmemory"] = procmemory
+            doc['procmemory'] = procmemory
 
         self.do_index(doc)
 
--- cuckoo/reporting/misp.py
+++ cuckoo/reporting/misp.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2016-2019 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
 import logging
 import os.path
 import shlex
@@ -16,34 +15,35 @@ from cuckoo.common.whitelist import (
 
 log = logging.getLogger(__name__)
 
+
 class MISP(Report):
     """Enrich MISP with Cuckoo results."""
 
     def sample_hashes(self, results, event):
-        if results.get("target", {}).get("file", {}):
-            f = results["target"]["file"]
+        if results.get('target', {}).get('file', {}):
+            f = results['target']['file']
             self.misp.add_hashes(
                 event,
-                category="Payload delivery",
-                filename=f["name"],
-                md5=f["md5"],
-                sha1=f["sha1"],
-                sha256=f["sha256"],
-                comment="File submitted to Cuckoo",
+                category='Payload delivery',
+                filename=f['name'],
+                md5=f['md5'],
+                sha1=f['sha1'],
+                sha256=f['sha256'],
+                comment='File submitted to Cuckoo',
             )
 
     def all_urls(self, results, event):
         """All of the accessed URLS as per the PCAP."""
         urls = set()
-        for protocol in ("http_ex", "https_ex"):
-            for entry in results.get("network", {}).get(protocol, []):
-                if is_whitelisted_mispdomain(entry["host"]):
+        for protocol in ('http_ex', 'https_ex'):
+            for entry in results.get('network', {}).get(protocol, []):
+                if is_whitelisted_mispdomain(entry['host']):
                     continue
-                if is_whitelisted_mispdomain(entry["host"]):
+                if is_whitelisted_mispdomain(entry['host']):
                     continue
 
-                url = "%s://%s%s" % (
-                      entry["protocol"], entry["host"], entry["uri"])
+                url = '%s://%s%s' % (
+                      entry['protocol'], entry['host'], entry['uri'])
 
                 if not is_whitelisted_mispurl(url):
                     urls.add(url)
@@ -52,18 +52,18 @@ class MISP(Report):
 
     def domain_ipaddr(self, results, event):
         domains, ips = {}, set()
-        for domain in results.get("network", {}).get("domains", []):
-            if is_whitelisted_mispip(domain["ip"]):
+        for domain in results.get('network', {}).get('domains', []):
+            if is_whitelisted_mispip(domain['ip']):
                 continue
 
-            if is_whitelisted_mispdomain(domain["domain"]):
+            if is_whitelisted_mispdomain(domain['domain']):
                 continue
 
-            domains[domain["domain"]] = domain["ip"]
-            ips.add(domain["ip"])
+            domains[domain['domain']] = domain['ip']
+            ips.add(domain['ip'])
 
         ipaddrs = set()
-        for ipaddr in results.get("network", {}).get("hosts", []):
+        for ipaddr in results.get('network', {}).get('hosts', []):
             if ipaddr not in ips and not is_whitelisted_mispip(ipaddr):
                 ipaddrs.add(ipaddr)
 
@@ -71,140 +71,139 @@ class MISP(Report):
         self.misp.add_ipdst(event, sorted(list(ipaddrs)))
 
     def family(self, results, event):
-        for config in results.get("metadata", {}).get("cfgextr", []):
+        for config in results.get('metadata', {}).get('cfgextr', []):
             self.misp.add_detection_name(
-                event, config["family"], "External analysis"
+                event, config['family'], 'External analysis'
             )
-            for cnc in config.get("cnc", []):
+            for cnc in config.get('cnc', []):
                 self.misp.add_url(event, cnc)
-            for url in config.get("url", []):
+            for url in config.get('url', []):
                 self.misp.add_url(event, url)
-            for mutex in config.get("mutex", []):
+            for mutex in config.get('mutex', []):
                 self.misp.add_mutex(event, mutex)
-            for user_agent in config.get("user_agent", []):
+            for user_agent in config.get('user_agent', []):
                 self.misp.add_useragent(event, user_agent)
 
     def signature(self, results, event):
-        for sig in results.get("signatures", []):
+        for sig in results.get('signatures', []):
 
             marks = []
 
-            if sig["ttp"]:
-                marks.append("%s" % ", ".join(sig["ttp"]))
+            if sig['ttp']:
+                marks.append('%s' % ', '.join(sig['ttp']))
 
-            for mark in sig.get("marks", []):
-                if mark["type"] == "generic":
+            for mark in sig.get('marks', []):
+                if mark['type'] == 'generic':
                     marks.append(
-                        "%s %s" % (mark.get("parent_process", ""),
-                                   mark.get("martian_process", ""))
+                        '%s %s' % (mark.get('parent_process', ''),
+                                   mark.get('martian_process', ''))
                     )
                     marks.append(
-                        "%s %s" % (mark.get("reg_key", ""),
-                                   mark.get("reg_value", ""))
+                        '%s %s' % (mark.get('reg_key', ''),
+                                   mark.get('reg_value', ''))
                     )
                     marks.append(
-                        "%s %s" % (mark.get("option", ""),
-                                   mark.get("value", ""))
+                        '%s %s' % (mark.get('option', ''),
+                                   mark.get('value', ''))
                     )
-                    marks.append("%s" % mark.get("domain", ""))
-                    marks.append("%s" % mark.get("description", ""))
-                    marks.append("%s" % mark.get("host", ""))
+                    marks.append('%s' % mark.get('domain', ''))
+                    marks.append('%s' % mark.get('description', ''))
+                    marks.append('%s' % mark.get('host', ''))
 
-                elif mark["type"] == "call":
-                    if not mark["call"]["api"] in marks:
-                        marks.append(mark["call"]["api"])
+                elif mark['type'] == 'call':
+                    if not mark['call']['api'] in marks:
+                        marks.append(mark['call']['api'])
 
-                elif mark["type"] == "config":
-                    marks.append(mark["config"].get("url", ""))
+                elif mark['type'] == 'config':
+                    marks.append(mark['config'].get('url', ''))
 
                 else:
-                    marks.append(mark[mark["type"]])
+                    marks.append(mark[mark['type']])
 
-            markslist = ", ".join([x for x in marks if x and x != " "])
+            markslist = ', '.join([x for x in marks if x and x != ' '])
 
-            data = "%s - (%s)" % (sig["description"], markslist)
+            data = '%s - (%s)' % (sig['description'], markslist)
             self.misp.add_internal_comment(event, data)
-            for att, description in sig["ttp"].items():
+            for att, description in sig['ttp'].items():
                 if not description:
-                    log.warning("Description for %s is not found", att)
+                    log.warning('Description for %s is not found', att)
                     continue
 
                 self.misp.add_internal_comment(
-                    event, "TTP: %s, short: %s" % (att, description["short"])
+                    event, 'TTP: %s, short: %s' % (att, description['short'])
                 )
 
     def run(self, results):
         """Submit results to MISP.
+
         @param results: Cuckoo results dict.
         """
-        url = self.options.get("url")
-        apikey = self.options.get("apikey")
-        mode = shlex.split(self.options.get("mode") or "")
-        score = results.get("info", {}).get("score", 0)
-        upload_sample = self.options.get("upload_sample")
+        url = self.options.get('url')
+        apikey = self.options.get('apikey')
+        mode = shlex.split(self.options.get('mode') or '')
+        score = results.get('info', {}).get('score', 0)
+        upload_sample = self.options.get('upload_sample')
 
-        if results.get("target", {}).get("category") == "file":
-            f = results.get("target", {}).get("file", {})
-            hash_whitelisted = is_whitelisted_misphash(f["md5"]) or \
-                               is_whitelisted_misphash(f["sha1"]) or \
-                               is_whitelisted_misphash(f["sha256"])
+        if results.get('target', {}).get('category') == 'file':
+            f = results.get('target', {}).get('file', {})
+            hash_whitelisted = is_whitelisted_misphash(f['md5']) or is_whitelisted_misphash(f['sha1']) or is_whitelisted_misphash(f['sha256'])
 
             if hash_whitelisted:
                 return
 
-        if score < self.options.get("min_malscore", 0):
+        if score < self.options.get('min_malscore', 0):
             return
 
         if not url or not apikey:
             raise CuckooProcessingError(
-                "Please configure the URL and API key for your MISP "
-                "instance."
+                'Please configure the URL and API key for your MISP '
+                'instance.'
             )
 
         with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
+            warnings.simplefilter('ignore')
             import pymisp
 
-        self.misp = pymisp.PyMISP(url, apikey, False, "json")
+        self.misp = pymisp.PyMISP(url, apikey, False, 'json')
 
         # Get default settings for a new event
-        distribution = self.options.get("distribution") or 0
-        threat_level = self.options.get("threat_level") or 4
-        analysis = self.options.get("analysis") or 0
-        tag = self.options.get("tag") or "Cuckoo"
+        distribution = self.options.get('distribution') or 0
+        threat_level = self.options.get('threat_level') or 4
+        analysis = self.options.get('analysis') or 0
+        tag = self.options.get('tag') or 'Cuckoo'
 
         event = self.misp.new_event(
             distribution=distribution,
             threat_level_id=threat_level,
             analysis=analysis,
-            info="Cuckoo Sandbox analysis #%d" % self.task["id"]
+            info='Cuckoo Sandbox analysis #%d' % self.task['id']
         )
 
         # Add a specific tag to flag Cuckoo's event
         if tag:
-            mispresult = self.misp.tag(event["Event"]["uuid"], tag)
-            if mispresult.has_key("message"):
-                log.debug("tag event: %s" % mispresult["message"])
+            mispresult = self.misp.tag(event['Event']['uuid'], tag)
+            if 'message' in mispresult:
+                log.debug('tag event: %s' % mispresult['message'])
 
         if upload_sample:
-            target = results.get("target", {})
-            if target.get("category") == "file" and target.get("file"):
+            target = results.get('target', {})
+            if target.get('category') == 'file' and target.get('file'):
                 self.misp.upload_sample(
-                    filename=os.path.basename(self.task["target"]),
-                    filepath_or_bytes=self.task["target"],
-                    event_id=event["Event"]["id"],
-                    category="External analysis",
+                    filename=os.path.basename(self.task['target']),
+                    filepath_or_bytes=self.task['target'],
+                    event_id=event['Event']['id'],
+                    category='External analysis',
                 )
 
         self.signature(results, event)
 
-        if "hashes" in mode:
+        if 'hashes' in mode:
             self.sample_hashes(results, event)
 
-        if "url" in mode:
+        if 'url' in mode:
             self.all_urls(results, event)
 
-        if "ipaddr" in mode:
+        if 'ipaddr' in mode:
             self.domain_ipaddr(results, event)
 
         self.family(results, event)
